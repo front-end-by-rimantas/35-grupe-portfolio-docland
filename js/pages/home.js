628571175e0a3051c617a043ce2ece61f81e4ebb
Rugile <rugilestasionyte@gmail.com>
2022-04-27 01:32:00 +0300

    syllabus list opens

--- js/pages/home.js
+++ js/pages/home.js
@@ -21,6 +21,22 @@
 /*  course slide ends here */
 
 /*  course syllabus starts here */
+const syllabusDOM = document.querySelectorAll('.syllabus-container');
+const syllabusRowDOM = document.querySelectorAll('.syllabus-row');
+const syllabusListDOM = document.querySelectorAll('.syllabus-list');
+const syllabusUpDOM = document.querySelector('.syllabus-angle-up');
+const syllabusDownDOM = document.querySelectorAll('.syllabus-angle-down');
+
+
+for (let i = 0; i < syllabusRowDOM.length; i++){
+    syllabusRowDOM[i].addEventListener('click', function(){
+        syllabusListDOM[i].classList.toggle('syllabus-list-open')
+    })
+}
+
+    
+
+
 /*  course syllabus ends here */
 
 /*  course benefits starts here */
